b463b53774c13bb261fcb90e03b9f3d0859ebf54
minja <zakazserov@gmail.com>
2019-12-07 15:29:22 +0300

    add path to test

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-from "3DPredictor/source/ChiPSeqReader.py" import ChiPSeqReader
+import sys
+import os
+source_path = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])),
+                           "3Dpredictor/source")
+print (source_path)
+sys.path.append(source_path)
+from ChiPSeqReader import ChiPSeqReader
 
 print("Hello world")
+print("Hello world2")
